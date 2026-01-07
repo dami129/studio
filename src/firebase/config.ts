b8b58924271ef3cbbfb5ce8708dc9cf9c12e21de
DAMIYAAA <damithabandara123@gmail.com>
2026-01-07 10:47:02 +0000

    Try fixing this error: `Runtime FirebaseError: Firebase: Error (auth/inv

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,12 @@
 // src/firebase/config.ts
 import type { FirebaseOptions } from 'firebase/app';
 
-const firebaseConfig: FirebaseOptions = JSON.parse(
-  process.env.NEXT_PUBLIC_FIREBASE_CONFIG || '{}'
-);
+const firebaseConfigString = process.env.NEXT_PUBLIC_FIREBASE_CONFIG;
+
+if (!firebaseConfigString) {
+    throw new Error('NEXT_PUBLIC_FIREBASE_CONFIG is not set');
+}
+
+const firebaseConfig: FirebaseOptions = JSON.parse(firebaseConfigString);
 
 export default firebaseConfig;
